cbea000cff85d60cd9a9d85c0948fc013ccfdeaa
ambershukla1998 <coolambershukla@gmail.com>
2025-03-20 18:54:13 +0530

    Initial commit for Medical Chatbot project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Amber Shukla',
+    author_email= 'amber.shukla.165@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
